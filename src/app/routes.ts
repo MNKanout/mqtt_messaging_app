6dff89e97994dc1f66c45d2116ef47c0d59e07ee
Nour <mnkanout@gmail.com>
2023-09-13 14:35:42 +0200

    Added routing to the messaging app

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,11 +2,13 @@ import { Route } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { MessagingComponent } from './messaging/messaging.component';
 
 
 const routes: Route[] = [
-    {path:'', component:HomeComponent},
-    {path:'login', component:LoginComponent}
+    {path:'', component: HomeComponent},
+    {path:'login', component: LoginComponent},
+    {path:'messaging', component: MessagingComponent},
   ]
   
 export {routes};
