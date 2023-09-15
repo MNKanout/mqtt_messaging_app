03375c34aa741442b5490039394039151d370069
Nour <mnkanout@gmail.com>
2023-09-15 14:19:03 +0200

    Fix test setup

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { LoginComponent } from './login.component';
+
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,7 +10,8 @@ describe('LoginComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [LoginComponent]
+      declarations: [LoginComponent],
+      imports:[RouterTestingModule],
     });
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
