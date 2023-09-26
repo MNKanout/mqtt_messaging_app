be88362fc4108baf390d920b1916f61140b8ac4d
Nour <mnkanout@gmail.com>
2023-09-26 15:16:46 +0200

    Added "should contain h3 welcome tag" test

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -19,5 +20,11 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain h3 welcome tag', () => {
+    const h3Elem: HTMLHeadElement = fixture.debugElement.
+    query(By.css('h3')).nativeElement;
+    expect(h3Elem.innerHTML).toBe('Welcome to MQTT messaging app');
   });
 });
