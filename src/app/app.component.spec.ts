e1c361bf526f1adcd61bf63f8211e6bd2f17575a
Nour <mnkanout@gmail.com>
2023-09-26 12:59:42 +0200

    Removed the render title test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,11 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('mqtt_messeging');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('mqtt_messeging app is running!');
-  });
 });
