4909edbe11116db192fc790687bbb2360d1039b7
Nour <mnkanout@gmail.com>
2023-09-27 10:59:57 +0200

    Added "should be at /login route" test

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,17 +1,21 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
 
+// Local
 import { LoginComponent } from './login.component';
+import {routes} from '../routes'
 
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [LoginComponent],
-      imports:[RouterTestingModule],
+      imports:[RouterTestingModule.withRoutes(routes)],
     });
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
@@ -21,4 +25,13 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it ('should be at /login route', fakeAsync(()=>{
+    const router: Router = TestBed.inject(Router);
+    router.initialNavigation();
+    router.navigate(['login']);
+    tick();
+    const currentRoute = router.routerState.snapshot.url;
+    expect(currentRoute).toBe('/login');
+  }));
 });
