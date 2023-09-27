c5d458fb1aea55e8d5a153e96c499c42a4078882
Nour <mnkanout@gmail.com>
2023-09-27 13:55:31 +0200

    Added 404 page not found!

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,6 +3,7 @@ import { Route } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { MessagingComponent } from './messaging/messaging.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const usernameRoutingVariable = 'username';
 
@@ -10,4 +11,5 @@ export const routes: Route[] = [
     {path:'', component: HomeComponent},
     {path:'login', component: LoginComponent},
     {path:'messaging/:'+ usernameRoutingVariable, component: MessagingComponent},
+    {path:'**', component:NotFoundComponent},
   ]
