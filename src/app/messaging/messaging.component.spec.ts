97b8cba80d2083c8f6e175d032bf8af432b4e379
Nour <mnkanout@gmail.com>
2023-09-29 11:30:00 +0200

    Added dependency injection dependencies

--- src/app/messaging/messaging.component.spec.ts
+++ src/app/messaging/messaging.component.spec.ts
@@ -1,21 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+// Third party
+import { RouterTestingModule } from '@angular/router/testing';
+import { ActivatedRoute} from '@angular/router';
+import {createSpyFromClass} from 'jasmine-auto-spies'; 
+
+// local
 import { MessagingComponent } from './messaging.component';
+import { MessagingService } from '../messaging.service';
+import { usernameRoutingVariable } from '../routes';
 
-// describe('MessagingComponent', () => {
-//   let component: MessagingComponent;
-//   let fixture: ComponentFixture<MessagingComponent>;
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [MessagingComponent]
-//     });
-//     fixture = TestBed.createComponent(MessagingComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+describe('MessagingComponent', () => {
+  let component: MessagingComponent;
+  let fixture: ComponentFixture<MessagingComponent>;
+  let route: ActivatedRoute;
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
-// });
+  beforeEach(() => {
+
+    const messagingServiceSpy = createSpyFromClass(MessagingService);
+
+    TestBed.configureTestingModule({
+      declarations: [MessagingComponent],
+      providers: [
+        {provide: MessagingService, useValue: messagingServiceSpy},
+      ],
+      imports: [
+        RouterTestingModule,
+      ],
+    });
+    fixture = TestBed.createComponent(MessagingComponent);
+    component = fixture.componentInstance;
+
+    fixture.detectChanges();
+    route = TestBed.inject(ActivatedRoute);
+  });
+
+  it('should create', () => {
+    const spyRoute = spyOn(route.snapshot.paramMap, 'get')
+    spyRoute.and.returnValue(usernameRoutingVariable);
+    expect(component).toBeTruthy();
+  });
+});
