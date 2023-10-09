848a7f9f376a40a2e49fae70fcc4b3be8184d949
Nour <mnkanout@gmail.com>
2023-10-09 16:50:47 +0200

    Added "Should connect" test

--- src/app/messaging.service.spec.ts
+++ src/app/messaging.service.spec.ts
@@ -1,16 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 
+// Third party
+import { MqttModule, MqttService } from 'ngx-mqtt';
+import { createSpyFromClass } from 'jasmine-auto-spies';
+
+// local 
 import { MessagingService } from './messaging.service';
-import { IMqttServiceOptions, MqttModule } from 'ngx-mqtt';
 import { connection } from './mqtt.config';
 
 describe('MessagingService', () => {
   let service: MessagingService;
+  let mqttServiceSpy = createSpyFromClass(MqttService);
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports:[
         MqttModule.forRoot(connection),
+      ],
+      providers:[
+        {provide: MqttService, useValue:mqttServiceSpy},
       ]
     });
     service = TestBed.inject(MessagingService);
@@ -18,5 +26,13 @@ describe('MessagingService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('Should connect',()=>{
+    // Act
+    service.connect();
+
+    // Assert
+    expect(mqttServiceSpy.connect).toHaveBeenCalled();
   });
 });
