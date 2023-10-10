cb9180ee5be35cfdaa3a406fd6f63a73c24b14ae
Nour <mnkanout@gmail.com>
2023-10-10 11:37:16 +0200

    Added "Should get connection status" test

--- src/app/messaging.service.spec.ts
+++ src/app/messaging.service.spec.ts
@@ -1,13 +1,36 @@
 import { TestBed } from '@angular/core/testing';
 
 // Third party
-import { IMqttMessage, MqttModule, MqttService } from 'ngx-mqtt';
+import { IMqttMessage, MqttModule, MqttService, MqttConnectionState } from 'ngx-mqtt';
 import { createSpyFromClass } from 'jasmine-auto-spies';
+import { subscribeSpyTo } from '@hirez_io/observer-spy';
 import { Subject } from 'rxjs';
+import { AddObservableSpyMethods, ValueConfig, ValueConfigPerCall } from '@hirez_io/auto-spies-core';
 
 // local 
 import { MessagingService } from './messaging.service';
 import { connection } from './mqtt.config';
+
+class SubjectMqttConnectionState extends Subject<MqttConnectionState> implements AddObservableSpyMethods<MqttConnectionState> {
+  nextWith(value?: MqttConnectionState | undefined): void {
+    throw new Error('Method not implemented.');
+  }
+  nextOneTimeWith(value?: MqttConnectionState | undefined): void {
+    throw new Error('Method not implemented.');
+  }
+  nextWithValues(valuesConfigs: ValueConfig<MqttConnectionState>[]): void {
+    throw new Error('Method not implemented.');
+  }
+  nextWithPerCall(valuesPerCall?: ValueConfigPerCall<MqttConnectionState>[] | undefined): Subject<MqttConnectionState>[] {
+    throw new Error('Method not implemented.');
+  }
+  throwWith(value: any): void {
+    throw new Error('Method not implemented.');
+  }
+  returnSubject(): Subject<MqttConnectionState> {
+    throw new Error('Method not implemented.');
+  }
+};
 
 describe('MessagingService', () => {
   let service: MessagingService;
@@ -67,5 +90,22 @@ describe('MessagingService', () => {
     // Assert
     expect(mqttServiceSpy.observe).toHaveBeenCalledOnceWith('test_topic');
     expect(subscribe$).toEqual(new Subject<IMqttMessage>());
+  });
+
+  it('Should get connection status', ()=>{
+    // Arrange
+    const state = new SubjectMqttConnectionState();
+    mqttServiceSpy.state = state;
+
+    // Act
+    const connectionStatus$ = service.getConnectedStatus();
+    const spySubscriber = subscribeSpyTo(connectionStatus$);
+    state.next(MqttConnectionState.CONNECTED);
+    state.next(MqttConnectionState.CONNECTED);
+    state.next(MqttConnectionState.CONNECTING);
+    state.next(MqttConnectionState.CLOSED);
+
+    // Assert
+    expect(spySubscriber.getValues()).toEqual([true, false]);
   });
 });
