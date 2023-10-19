1e1b10cfd4acb813d6b9205ee22c071e15925d22
Nour Kanout <mnkanout@gmail.com>
2023-10-19 22:16:43 +0200

    Added "Should navigate to messaging page with valid username" test

---
+++ cypress/e2e/messaging_template.cy.ts
@@ -0,0 +1,8 @@
+describe('messaging template spec', () => {
+
+  beforeEach(()=>{
+    cy.visit('localhost:4200/messaging/testUser');
+  });
+
+  it('Should navigate to messaging page with valid username', () => {});
+});
