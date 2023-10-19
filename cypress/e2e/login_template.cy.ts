a133fd1bfea5f6bee753c8431e1e1b15a23013d5
Nour Kanout <mnkanout@gmail.com>
2023-10-19 16:46:28 +0200

    Added "Should notify when logging in with invalid username" test.

--- cypress/e2e/login_template.cy.ts
+++ cypress/e2e/login_template.cy.ts
@@ -5,6 +5,33 @@ describe('Login template', () => {
 
   it('Should navigate to login page', () => {});
 
+  it('Should notify when logging in with empty username', () => {
+
+    // Act
+    cy.get<HTMLButtonElement>('button').click();
+    cy.get('simple-snack-bar')
+    .find('.mat-mdc-snack-bar-label')
+    .invoke('text')
+    
+    // Assert
+    .should('include','Please enter a username!');
+  });
+
+  it('Should notify when logging in with invalid username', () => {
+    // Arrange
+    const user: string = 'test_user';
+
+    // Act
+    cy.get<HTMLInputElement>('input').type(user);
+    cy.get<HTMLButtonElement>('button').click();
+
+    cy.get('simple-snack-bar')
+    .find('.mat-mdc-snack-bar-label')
+    .invoke('text')
+    // Assert
+    .should('include','Invalid Username')
+  });
+
   it('Should navigate to messaging page when logging in with valid username', () => {
     // Arrange
     const user: string = 'testUser';
